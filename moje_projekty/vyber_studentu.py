f77c361f0a8c9a5bc210628ffeffe82d8c89cc14
foblezar <st025554@student.cichnovabrno.cz>
2025-11-14 10:39:31 +0100

    ctenisoobuoru

---
+++ moje_projekty/vyber_studentu.py
@@ -0,0 +1,26 @@
+import random
+
+
+def vyber_studentu(n):
+    cesta = "2. prace_se_soubory\data\studenti.txt"
+    with open(cesta, "r", encoding="utf-8") as file:
+        radky = file.readlines()
+      
+    jmena = []
+
+    for i in range(len(radky)):
+        jmena.append(radky[i].strip()) 
+
+    for i in range(n):
+        print(jmena[random.randint(0, len(jmena)-1)])
+
+n = int(input("Zadej pocet studentu: "))
+
+vyber_studentu(n)
+
+
+
+
+
+
+
