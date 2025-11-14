7c85d1efa6054f5763a29e580a3beb7325e97f34
foblezar <st025554@student.cichnovabrno.cz>
2025-11-14 11:37:02 +0100

    psani_do_souboru

---
+++ moje_projekty/psani_do_souboru.py
@@ -0,0 +1,16 @@
+import random
+
+mini = int(input("Zadej min: ")) 
+maxi = int(input("Zadej max: "))
+pocet = int(input("Zadej pocet: "))
+
+cesta = "2. prace_se_soubory\data\generovani.txt"
+cisla = []
+with open(cesta, "w", encoding="utf-8") as file:
+    for i in range(pocet):
+        cisla.append(random.randint(mini, maxi))
+
+    for i in range(len(cisla)):
+        file.write(cisla[i])
+
+
