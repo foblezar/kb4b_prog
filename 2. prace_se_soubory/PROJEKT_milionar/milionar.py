cda300622a29efd79e17a7c75bde4c8383937077
foblezar <st025554@student.cichnovabrno.cz>
2025-12-04 11:38:22 +0100

    PROJEKT_MILIONAR

---
+++ 2. prace_se_soubory/PROJEKT_milionar/milionar.py
@@ -0,0 +1,110 @@
+import random
+import csv
+import matplotlib.pyplot as plt
+
+def vypis_statistiky():
+    pocet_otazek = 0
+    pocet_easy = 0
+    pocet_medium = 0
+    pocet_hard = 0
+
+    kategorie_gk = 0
+    kategorie_e = 0
+    kategorie_s = 0
+    kategorie_g = 0
+    kategorie_h = 0
+
+    print("Jaký graf chceš vypsat?")
+    print("1. Poměr obtížností")
+    print("2. Poměr kategorií")
+    stat_choice = int(input("Zadej moznost: "))
+    if stat_choice == 1:
+        with open("2. prace_se_soubory\PROJEKT_milionar\quiz_questions.csv", "r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for line in reader:
+                pocet_otazek += 1
+                if line["difficulty"] == "easy":
+                    pocet_easy += 1
+                elif line["difficulty"] == "medium":
+                    pocet_medium += 1
+                elif line["difficulty"] == "hard":
+                    pocet_hard += 1
+
+            difficulties = ["easy", "medium", "hard"]
+            pocet_difficulties = [pocet_easy, pocet_medium, pocet_hard]
+        plt.bar(difficulties, pocet_difficulties)
+        plt.title("Poměr obtížností")
+        plt.show()
+        print(f"Počet otázek: {pocet_otazek}")
+    elif stat_choice == 2:
+        with open("2. prace_se_soubory\PROJEKT_milionar\quiz_questions.csv", "r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for line in reader:
+                if line["category"] == "General Knowledge":
+                    kategorie_gk += 1
+                elif line["category"] == "Entertainment":           ## OPRAVIT MA VICE KATEGORII
+                    kategorie_e += 1
+                elif line["category"] == "Science: Nature":      ## OPRAVIT MA VICE KATEGORII
+                    kategorie_sn += 1
+                elif line["category"] == "Geography":
+                    kategorie_g += 1
+                elif line["category"] == "History":
+                    kategorie_h += 1
+            
+            categories = ["General Knowledge", "Entertaiment: Books", "Science", "Geography", "History"]
+            pocet_categories = [kategorie_gk, kategorie_e, kategorie_s, kategorie_g, kategorie_h]
+        plt.bar(categories, pocet_categories)
+        plt.title("Poměr kategorií")
+        plt.show()
+
+
+
+
+def main_menu():
+    pokracovat = True
+    while pokracovat is True:
+        print("1. Statistiky otazek")
+        print("2. Vypis vsech vitezu")
+        print("3. Hrat hru")
+        print("0. Ukončit hru")
+        choice = int(input("Zadej moznost: "))
+        if choice == 1:
+            vypis_statistiky()
+        elif choice == 2:
+            vypis_vitezu()
+        elif choice == 3:
+            hlavni_hra()
+        else:
+            pokracovat = False
+
+
+
+
+
+
+print("Zadej zpusob prihlaseni: ")
+print("1. Registrace")
+print("2. Prihlaseni")
+choice = int(input("Zadej moznost: "))
+if choice == 1:
+    login = input("Zadej login: ")
+    password = input("Zadej password: ")
+    with open("2. prace_se_soubory\PROJEKT_milionar\logins.txt", "a", encoding="utf-8") as file:
+        file.write(str(f"{login} {password}\n"))
+        success = 1
+
+elif choice == 2:
+    login = input("Zadej login: ")
+    password = input("Zadej password: ")
+    with open("2. prace_se_soubory\PROJEKT_milionar\logins.txt", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+        for line in lines:
+            control_login = line.split()
+            if login == control_login[0] and password == control_login[1]:
+                print("Přihlášení úspěšné")
+                success = 1
+   
+if success == 1:
+    main_menu()
+else:
+    print("Neúspěšné přihlášení")
